ca9dadd9ba055d686d3ef45c570749c7b17f5041
RayLiee <98361064+RayLiee@users.noreply.github.com>
2022-01-24 23:26:14 +0100

    Delete installer.bat

--- installer.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}"; "& {Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/R3nzTheCodeGOD/Spotify-Internal/master/scripts/install.ps1' | Invoke-Expression}"
-pause
-exit
